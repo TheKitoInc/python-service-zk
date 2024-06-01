973e08d1008dc3cec80aa099675b03d1ffc9b520
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-01 16:00:57 -0300

    Update localTime.py

--- localTime.py
+++ localTime.py
@@ -4,20 +4,20 @@ from datetime import datetime
 
 
 def getLocalTimeZoneOffset():
-        try:
-                timeZoneOffset = -time.timezone
-                sys.stderr.write("timeZoneOffset: " + str(timeZoneOffset) + "\n")
-                return timeZoneOffset
-        except Exception as e:
-                print("getTimeZoneOffsetException : {}".format(e))
-                return None
+    try:
+        timeZoneOffset = -time.timezone
+        sys.stderr.write("timeZoneOffset: " + str(timeZoneOffset) + "\n")
+        return timeZoneOffset
+    except Exception as e:
+        print("getTimeZoneOffsetException : {}".format(e))
+        return None
 
 
 def getLocalTimeCurrent():
-        try:
-                timeCurrent = time.mktime(datetime.today().timetuple())
-                sys.stderr.write("timeCurrent: " + str(timeCurrent) + "\n")
-                return timeCurrent
-        except Exception as e:
-                print("getTimeCurrentException : {}".format(e))
-                return None
+    try:
+        timeCurrent = time.mktime(datetime.today().timetuple())
+        sys.stderr.write("timeCurrent: " + str(timeCurrent) + "\n")
+        return timeCurrent
+    except Exception as e:
+        print("getTimeCurrentException : {}".format(e))
+        return None
