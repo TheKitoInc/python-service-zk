f8a8c95cea1b84fed40e2ea62adefa0edce0878f
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-01 15:36:32 -0300

    Update deviceInfo.py

--- deviceInfo.py
+++ deviceInfo.py
@@ -2,29 +2,32 @@ import sys
 import time
 from localTime import getLocalTimeCurrent
 
+
 def getDeviceTime(deviceConnection):
-        try:                
+        try:
                 timeDevice = time.mktime(deviceConnection.get_time().timetuple())
                 sys.stderr.write("timeCurrent: " + str(timeDevice) + "\n")
                 return timeDevice
         except Exception as e:                
-                print ("getDeviceTimeException : {}".format(e))
+                print("getDeviceTimeException : {}".format(e))
                 return None
-        
+
+
 def getDeviceTimeOffset(deviceConnection):
-        try:                        
+        try:
                 timeOffset = getDeviceTime(deviceConnection) - getLocalTimeCurrent() 
                 sys.stderr.write("timeOffset: " + str(timeOffset) + "\n")
                 return timeOffset
         except Exception as e:                
-                print ("getTimeOffsetException : {}".format(e))
+                print("getTimeOffsetException : {}".format(e))
                 return None
-        
+
+
 def getDeviceSerial(deviceConnection):
         try:
                 deviceSerial = deviceConnection.get_serialnumber()
                 sys.stderr.write("deviceSerial: " + deviceSerial + "\n")  
                 return deviceSerial                  
         except Exception as e:                
-                print ("getDeviceSerial : {}".format(e))     
+                print("getDeviceSerial : {}".format(e))     
                 return None
