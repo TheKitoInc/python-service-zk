fd3ef69e99daba2896cfac309520dad05eeaaccf
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-01 18:45:50 -0300

    Update logger.py

--- logger.py
+++ logger.py
@@ -9,23 +9,23 @@ def getLogger():
 
 
 def debug(title, content):
-    logger.debug('%s: %s', title, content)
+    logger.debug('%s:%s', title, content)
 
 
 def info(title, content):
-    logger.info('%s: %s', title, content)
+    logger.info('%s:%s', title, content)
 
 
 def warn(title, content):
-    logger.warn('%s: %s', title, content)
+    logger.warn('%s:%s', title, content)
 
 
 def error(title, content):
-    logger.error('%s: %s', title, content)
+    logger.error('%s:%s', title, content)
 
 
 def fatal(title, content):
-    logger.fatal('%s: %s', title, content)
+    logger.fatal('%s:%s', title, content)
 
 
 debug('Status', 'Logger Init')
