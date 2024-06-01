c52e9cece77a0daf57a529cab48c4e25d33f3c1a
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-01 16:00:52 -0300

    Update deviceInfo.py

--- deviceInfo.py
+++ deviceInfo.py
@@ -4,30 +4,30 @@ from localTime import getLocalTimeCurrent
 
 
 def getDeviceTime(deviceConnection):
-        try:
-                timeDevice = time.mktime(deviceConnection.get_time().timetuple())
-                sys.stderr.write("timeCurrent: " + str(timeDevice) + "\n")
-                return timeDevice
-        except Exception as e:
-                print("getDeviceTimeException : {}".format(e))
-                return None
+    try:
+        timeDevice = time.mktime(deviceConnection.get_time().timetuple())
+        sys.stderr.write("timeCurrent: " + str(timeDevice) + "\n")
+        return timeDevice
+    except Exception as e:
+        print("getDeviceTimeException : {}".format(e))
+        return None
 
 
 def getDeviceTimeOffset(deviceConnection):
-        try:
-                timeOffset = getDeviceTime(deviceConnection) - getLocalTimeCurrent()
-                sys.stderr.write("timeOffset: " + str(timeOffset) + "\n")
-                return timeOffset
-        except Exception as e:
-                print("getTimeOffsetException : {}".format(e))
-                return None
+    try:
+        timeOffset = getDeviceTime(deviceConnection) - getLocalTimeCurrent()
+        sys.stderr.write("timeOffset: " + str(timeOffset) + "\n")
+        return timeOffset
+    except Exception as e:
+        print("getTimeOffsetException : {}".format(e))
+        return None
 
 
 def getDeviceSerial(deviceConnection):
-        try:
-                deviceSerial = deviceConnection.get_serialnumber()
-                sys.stderr.write("deviceSerial: " + deviceSerial + "\n")
-                return deviceSerial
-        except Exception as e:
-                print("getDeviceSerial : {}".format(e))
-                return None
+    try:
+        deviceSerial = deviceConnection.get_serialnumber()
+        sys.stderr.write("deviceSerial: " + deviceSerial + "\n")
+        return deviceSerial
+    except Exception as e:
+        print("getDeviceSerial : {}".format(e))
+        return None
