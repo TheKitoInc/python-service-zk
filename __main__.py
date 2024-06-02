3b33160bea2cbcd542347e27afa2475983296bc4
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-01 21:07:04 -0300

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,6 +1,17 @@
 import sys
 from app import main
+import argparse
 
+parser = argparse.ArgumentParser(description='ZKDevice Proxy Service')
+parser.add_argument('host', help='Device Hostname / IP Address')
+parser.add_argument('--port', help='Device port', type=int, default=4370)
+parser.add_argument('--timeout', help='Connection timeout',
+                    type=int, default=5)
+parser.add_argument('--password', help='Device password', type=int, default=0)
+parser.add_argument(
+    '--UDP', action=argparse.BooleanOptionalAction, default=False)
+parser.add_argument(
+    '--ping', action=argparse.BooleanOptionalAction, default=True)
 
 if __name__ == '__main__':
-    sys.exit(main())
+    sys.exit(main(parser.parse_args()))
